c74a1b153c01df67d67973bb1984d49fa3c73e49
Evan <evanz1215@gmail.com>
2024-09-19 09:32:45 +0800

    加入專案檔案。

---
+++ src/WPFDynamicTheme.Core/ThemeManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace WPFDynamicTheme.Core;
+
+public class ThemeManager
+{
+    private Dictionary<string, ResourceDictionary> _themes = new();
+
+    public void RegisterTheme(string themeName, string assemblyName, string resourcePath)
+    {
+        string uri = $"/{assemblyName};component/{resourcePath}";
+
+        ResourceDictionary resource = new ResourceDictionary();
+        resource.Source = new Uri(uri, UriKind.RelativeOrAbsolute);
+
+        _themes.Add(themeName, resource);
+    }
+
+    public void ApplyTheme(string themeName)
+    {
+        ResourceDictionary resource = _themes[themeName];
+
+        foreach (var theme in _themes)
+        {
+            Application.Current.Resources.MergedDictionaries.Remove(theme.Value);
+        }
+
+        Application.Current.Resources.MergedDictionaries.Add(resource);
+    }
+}
---
+++ src/WPFDynamicTheme/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using WPFDynamicTheme.Core;
+
+namespace WPFDynamicTheme;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private ThemeManager _themeManager;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        _themeManager = new ThemeManager();
+
+        _themeManager.RegisterTheme("Light", "WPFDynamicTheme.Resources", "LightTheme.xaml");
+        _themeManager.RegisterTheme("Dark", "WPFDynamicTheme.Resources", "DarkTheme.xaml");
+
+        Switch.Checked += Switch_Checked;
+        Switch.Unchecked += Switch_Unchecked;
+    }
+
+    private void Switch_Checked(object sender, RoutedEventArgs e)
+    {
+        _themeManager.ApplyTheme("Light");
+    }
+
+    private void Switch_Unchecked(object sender, RoutedEventArgs e)
+    {
+        _themeManager.ApplyTheme("Dark");
+    }
+}
